f6f91714e69529439991a43ebbe373fd299aa0c2
Nihal Muhammed <nihalmuhammed7025@gmail.com>
2023-02-11 19:46:52 +0530

    second

--- src/Config/axios.js
+++ src/Config/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/'
-    // baseURL: 'https://commenter.bristlesweb.club/'
+    // baseURL: 'http://localhost:5000/'
+    baseURL: 'https://we-share.club/'
 })
 
 
